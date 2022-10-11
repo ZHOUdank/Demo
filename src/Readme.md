08c5ac827462dcf299aa875d03cc03b5e224ac26
lkj <dank13@163.com>
2022-10-12 00:41:32 +0800

    学习测试

---
+++ src/Readme.md
@@ -0,0 +1,5 @@
+git init
+git add README.md (没有建README.md的用git add .)
+git commit -m "first commit"
+git remote add origin https://github.com/~~~~~~/~~~~~.git
+git push -u origin master
---
+++ src/com/dank/Test1.java
@@ -0,0 +1,38 @@
+package com.dank;
+
+/**
+ * @ClassName: test1
+ * @author: zyk
+ * @createTime 2022年10月12日 00:13:00
+ * @Description:
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        String s = "zyk";
+        String b = str(s);
+        System.out.println(str(s));
+        System.out.println("asd:"+b);
+    }
+
+
+    /**
+     * @title: str
+     * @author: zyk
+     * @updateTime: 2022/10/12 0:24
+     * @description:
+    */
+    public static String str(String a){
+        String b = "222"+"333"+a;
+        return b;
+    }
+    /**
+     * @title: sss
+     * @author: zyk
+     * @updateTime: 2022/10/12 0:23
+     * @description:
+     */
+    public static String sss(String b){
+        String c = "22";
+        return b;
+    }
+}
