dcc27750beda4640da2101795c8360207d833542
ZhouYuanKe <dank13@163.com>
2023-04-10 21:00:28 +0800

    冒泡，选择，快速，希尔排序

---
+++ src/com/dank/study/paixu.java
@@ -0,0 +1,68 @@
+package com.dank.study;
+
+/**
+ * @ClassName: paixu
+ * @author: zyk
+ * @createTime 2023年04月10日 20:25:00
+ * @Description:
+ */
+public class paixu {
+    //冒泡排序
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    //选择排序
+    public static void selectSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+        }
+    }
+    //插入排序
+    public static void insertSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int insertVal = arr[i];
+            int insertIndex = i - 1;
+            while (insertIndex >= 0 && insertVal < arr[insertIndex]) {
+                arr[insertIndex + 1] = arr[insertIndex];
+                insertIndex--;
+            }
+            arr[insertIndex + 1] = insertVal;
+        }
+    }
+    //希尔排序
+    public static void shellSort(int[] arr) {
+        int temp = 0;
+        int count = 0;
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                for (int j = i - gap; j >= 0; j -= gap) {
+                    if (arr[j] > arr[j + gap]) {
+                        temp = arr[j];
+                        arr[j] = arr[j + gap];
+                        arr[j + gap] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+
+}
